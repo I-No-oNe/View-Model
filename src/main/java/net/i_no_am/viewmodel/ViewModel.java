3b7e821349c818af341dc597e5af66b1c51c0e76
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2025-03-20 23:12:35 +0200

    Update to 1.0.0 * moved to use the new Version class * removed underwater vision

--- src/main/java/net/i_no_am/viewmodel/ViewModel.java
+++ src/main/java/net/i_no_am/viewmodel/ViewModel.java
@@ -4,6 +4,8 @@ import io.github.itzispyder.improperui.ImproperUIAPI;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.i_no_am.viewmodel.version.Version;
 import net.i_no_am.viewmodel.config.Config;
 import net.i_no_am.viewmodel.event.SecondMenuCallBack;
@@ -13,13 +15,16 @@ import org.lwjgl.glfw.GLFW;
 
 public class ViewModel implements ModInitializer, Global {
 
-    public static boolean isOutdated = false;
+    public static final String API = "https://api.github.com/repos/I-No-oNe/View-Model/releases/latest";
+    public static final String DOWNLOAD = "https://modrinth.com/mod/no-ones-view-model/versions";
     public static final KeyBinding BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding("binds.viewmodel.menu", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "binds.viewmodel"));
 
     @Override
     public void onInitialize() {
-        Version.checkUpdates();
         ImproperUIAPI.init(modId, ViewModel.class, screens);
+
+        WorldRenderEvents.AFTER_SETUP.register(context -> Version.create(API, DOWNLOAD).notifyUpdate(FabricLoader.getInstance().isDevelopmentEnvironment()));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             Config.loadConfig();
             while (BIND.wasPressed()) {
